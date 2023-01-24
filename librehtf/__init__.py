a9ab99756cf4d817ecd79551c588a4152655728b
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-23 22:21:00 -0500

    Create __init__.py

---
+++ librehtf/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import path
+from os import makedirs
+
+from flask import Flask
+
+from librehtf.auth import auth
+from librehtf.api import api
+from librehtf.db import init_app
+
+
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=path.join(app.instance_path, "librehtf.sqlite"),
+    )
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.update(test_config)
+    try:
+        makedirs(app.instance_path)
+    except OSError:
+        pass
+    init_app(app)
+    app.register_blueprint(auth)
+    app.register_blueprint(api)
+    app.add_url_rule("/", endpoint="index")
+    return app

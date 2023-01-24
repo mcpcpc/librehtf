04794946597269181f9988e4b13a15c83d2cc927
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-24 07:02:54 -0500

    add missing import

--- librehtf/auth.py
+++ librehtf/auth.py
@@ -8,6 +8,7 @@ from functools import wraps
 
 from flask import Blueprint
 from flask import current_app
+from flask import flash
 from flask import g
 from flask import redirect
 from flask import render_template

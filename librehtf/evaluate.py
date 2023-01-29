cf06fb4ff7309f15708cd0bed6c0c9cfdbb14556
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-29 09:29:41 -0500

    test

--- librehtf/evaluate.py
+++ librehtf/evaluate.py
@@ -10,10 +10,19 @@ from librehtf.db import get_db
 
 evaluate = Blueprint("evaluate", __name__)
 
-query = """
+query_devices = """
 SELECT
     device.name AS device_name,
     device.description AS device_description,
+    test.id AS test_id,
+    test.name AS test_name,
+    test.description AS test_description
+FROM device
+    INNER JOIN test ON test.device_id = device.id
+"""
+
+query_test_id = """
+SELECT
     test.id AS test_id,
     test.name AS test_name,
     test.description AS test_description,
@@ -25,10 +34,11 @@ SELECT
     operator.slug AS operator_slug,
     datatype.slug AS datatype_slug
 FROM device
-    INNER JOIN test ON test.device_id = device.id
     INNER JOIN task ON task.test_id = test.id
     INNER JOIN operator ON operator.id = task.operator_id
     INNER JOIN datatype ON datatype.id = task.datatype_id
+WHERE
+    test_id = ? 
 """
 
 
@@ -42,18 +52,24 @@ def measure(command: str):
 @evaluate.route("/evaluate", methods=("GET",))
 @login_required
 def index():
-    rows = get_db().execute(query).fetchall()
+    rows = get_db().execute(query_devices).fetchall()
     return render_template("evaluate.html", rows=rows)
 
 
 @evaluate.route("/evaluate/<int:test_id>", methods=("GET",))
 @login_required
-def run(test_id: int):
-    rows = get_db().execute(query + " WHERE test_id = ?", (test_id,)).fetchall()
+def results(test_id: int):
+    rows = get_db().execute(query_test_id, (test_id,)).fetchall()
+    if rows is None:
+        return "Invalid test identifier.", 400
     results = {}
     for row in rows:
-        measured = measure(row["task_command"])
-        if "measured" not in measured:
+        result = measure(row["task_command"])
+        if "measured" not in result:
             return "Invalid command.", 400
-        results[row["task_id"]] = measured
-    return results
+        result["test_name"] = row["test_name"]
+        result["task_name"] = row["task_name"]
+        result["task_description"] = row["task_description"]
+        result["observation"] = "INFO" # temporary
+        results[row["task_id"]] = result
+    return render_template("evaluate/results.html", results=results)

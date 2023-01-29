f32db6a981554bfa688ebcc25f202cc7254ead31
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-29 08:19:28 -0500

    Update

--- librehtf/evaluate.py
+++ librehtf/evaluate.py
@@ -52,6 +52,7 @@ def run(test_id: int):
     rows = get_db().execute(query + " WHERE test_id = ?", (test_id,)).fetchall()
     results = {}
     for row in rows:
+        print(dict(row))
         measured = measure(row["command"])
         if "measured" not in measured:
             return "Invalid command.", 400

e225858f73a4fac8e1c350e429077bdce15d95b2
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-01-28 21:57:45 -0500

    Update

--- librehtf/evaluate.py
+++ librehtf/evaluate.py
@@ -43,7 +43,7 @@ def index():
 def run(task_id: int):
     task = get_db().execute("SELECT * FROM task WHERE id = ?", (task_id,)).fetchone()
     measured = None
-    print(measured)
+    print(task["command"])
     cc = compile(task["command"], "<string>", "exec")
     exec(cc)
     return str(measured)

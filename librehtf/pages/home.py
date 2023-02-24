f4545114e40fcb13c24e037e4d48df9c23e224f6
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-02-24 17:54:38 -0500

    Update

--- librehtf/pages/home.py
+++ librehtf/pages/home.py
@@ -1,6 +1,7 @@
 from dash import register_page
 from dash_iconify import DashIconify
 from dash_mantine_components import Container
+from dash_mantine_components import Group
 from dash_mantine_components import Button
 from dash_mantine_components import PasswordInput
 from dash_mantine_components import Text
@@ -24,6 +25,20 @@ layout = Container(
                     placeholder="Password",
                     icon=DashIconify(icon="bi:shield-lock"),
                 ),
+                Group(
+                    position="right",
+                    children=[
+                        Button(
+                            id="login",
+                            children="Log In",
+                        ),
+                        Button(
+                            id="back",
+                            variant="transparent",
+                            children="Back",
+                        ),
+                    ],
+                )
             ]
         )
     ]

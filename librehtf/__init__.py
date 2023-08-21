fdbcb3fed646102a9867ae1c74a8863bc05cc35e
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-08-21 16:23:36 -0400

    update

--- librehtf/__init__.py
+++ librehtf/__init__.py
@@ -20,14 +20,23 @@ from librehtf.token import init_token
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
 
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(
+        __name__,
+        instance_relative_config=True,
+    )
     app.config.from_mapping(
         SECRET_KEY="dev",
         MAX_CONTENT_LENGTH=16000000,
-        DATABASE=path.join(app.instance_path, "librehtf.sqlite"),
+        DATABASE=path.join(
+            app.instance_path,
+            "librehtf.sqlite",
+        ),
     )
     if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_pyfile(
+        "config.py",
+        silent=True,
+    )
     else:
         app.config.update(test_config)
     try:

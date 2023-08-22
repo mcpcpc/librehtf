6a3214b7e50c05aa5a2db71208538d48c78509fd
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-08-22 06:56:23 -0400

    update

--- librehtf/schema.sql
+++ librehtf/schema.sql
@@ -1,76 +1,16 @@
 -- Initialize the database.
 -- Drop any existing data and create empty tables.
 
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS permission;
-DROP TABLE IF EXISTS role_permission;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS datatype;
+DROP TABLE IF EXISTS operator;
 DROP TABLE IF EXISTS device;
 DROP TABLE IF EXISTS test;
 DROP TABLE IF EXISTS task;
-DROP TABLE IF EXISTS datatype;
-DROP TABLE IF EXISTS operator;
 
-CREATE TABLE role (
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        slug TEXT UNIQUE NOT NULL,
-        description TEXT DEFAULT NULL,
-        active INTEGER NOT NULL DEFAULT 0,
-        content TEXT DEFAULT NULL
-);
-
-INSERT INTO role (slug, title) VALUES
-        ("admin", "administrator"),
-        ("functional", "functional"),
-        ("public", "public");
-
-CREATE TABLE permission (
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        slug TEXT UNIQUE NOT NULL,
-        description TEXT DEFAULT NULL,
-        active INTEGER NOT NULL DEFAULT 0,
-        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at DATETIME DEFAULT NULL,
-        content TEXT DEFAULT NULL
-);
-
-INSERT INTO permission (slug, title) VALUES
-        ("c", "create"),
-        ("r", "read"),
-        ("u", "update"),
-        ("d", "delete");
-
-CREATE TABLE role_permission (
-        role_id INTEGER NOT NULL,
-        permission_id INTEGER NOT NULL,
-        PRIMARY KEY(role_id, permission_id)
-        FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE NO ACTION ON UPDATE NO ACTION
-        FOREIGN KEY(permission_id) REFERENCES permission(id) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-INSERT INTO role_permission (role_id, permission_id) VALUES
-        (1, 1),
-        (1, 2),
-        (1, 3),
-        (1, 4),
-        (2, 1),
-        (2, 2),
-        (3, 2);
-
-CREATE TABLE user (
-        id INTEGER PRIMARY KEY,
-        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at DATETIME DEFAULT NULL, 
-        username TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL UNIQUE,
-        role_id INTEGER NOT NULL,
-        FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-INSERT INTO user (role_id, username, password) VALUES
-        (1, "admin", "pbkdf2:sha256:260000$gtvpYNx6qtTuY8rt$2e2a4172758fee088e20d915ac4fdef3bdb07f792e42ecb2a77aa5a72bedd5f5");
+INSERT INTO datatype (slug, title) VALUES
+        ("str", "string"),
+        ("int", "integer"),
+        ("float", "floating point");
 
 CREATE TABLE operator (
         id INTEGER PRIMARY KEY,
@@ -92,11 +32,6 @@ CREATE TABLE datatype (
         title TEXT UNIQUE NOT NULL,
         slug TEXT UNIQUE NOT NULL
 );
-
-INSERT INTO datatype (slug, title) VALUES
-        ("str", "string"),
-        ("int", "integer"),
-        ("float", "floating point");
 
 CREATE TABLE device (
         id INTEGER PRIMARY KEY,

c7f0f7dd6ef5a1843023de5e2b01d20aea07cec2
Michael Czigler <michaelczigler@icloud.com>
2023-08-22 13:58:58 -0400

    test navlink idea

--- librehtf/pages/evaluate.py
+++ librehtf/pages/evaluate.py
@@ -7,21 +7,55 @@ from dash import no_update
 from dash import Output
 from dash import register_page
 from dash_iconify import DashIconify
-from dash_mantine_components import Stack
-from dash_mantine_components import Text
-from dash_mantine_components import Title
+from dash.dcc import Store
+from dash_mantine_components import Col
+from dash_mantine_components import Grid
+from dash_mantine_components import Card
+from dash_mantine_components import Navbar
+from dash_mantine_components import NavLink
 
+from librehtf.db import get_db
 from librehtf.utils.plugin import MeasurementPlugin
 
-register_page(__name__, path="/evaluate/<device>")
+register_page(__name__, path="/evaluate")
 
-def layout(device: str = None):
+def layout(test: str = None, **parameters):
     return [
-        Stack(
-            p="xl",
+        Store(id="test", data=test),
+        Grid(
             children=[
-                Title("Evaluate"),
-                Text(""),
-            ]
+                Col(
+                    span=3,
+                    children=[
+                        Navbar(
+                            id="navbar",
+                            children=None,
+                        ),
+                    ],
+                ),
+                Col(
+                    span=9,
+                    id="tasklist",
+                ),
+            ],
         ),
+    ]
+
+@callback(
+    Output("navbar", "children"),
+    Input("test", "data"),
+)
+def update_navbar(test: str):
+    rows = get_db().execute(
+        "SELECT * FROM test"
+    ).fetchall()
+    if not rows:
+        return no_update
+    records = list(map(dict, rows))
+    return [
+        NavLink(
+            label=record["name"],
+            description=record["name"],
+        )
+        for record in records
     ]
